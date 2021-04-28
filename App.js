0c0683df805b4d2ac5c682afd48cca09169bae90
Oluwayimika Ojo-Bello <yimzika@gmail.com>
2021-04-28 16:09:52 +0000

    adding files

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ImageBackground } from 'react-native';
+import { NativeRouter,Link,Route } from 'react-router-native';
+import {Provider} from 'react-redux'
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import store from './store';
 
+const image = { uri: "https://images.unsplash.com/photo-1556155092-8707de31f9c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" };
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NativeRouter>
+          <View style={styles.container}>
+          <ImageBackground source={image} style={styles.image}>
+            
+            <View>
+              <StatusBar style="auto" />
+              <Text style={styles.textVertMarg}>Hello from 10758282 </Text>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </View>         
+            
+            
+          </ImageBackground>
+          </View>
+      </NativeRouter>
+    </Provider>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:'center',
+    flexDirection:'column',
+    width:'100%',
+    height:'100%',
+
   },
+  textVertMarg:{
+    marginVertical:20,
+    color:'white',
+    fontSize:30,
+    alignItems:'center',
+    justifyContent:'center',
+
+    fontWeight:"700",
+    marginLeft:110
+  },
+  image: {
+    flex: 1  ,
+    resizeMode: "cover",
+    justifyContent: "center",
+  }
 });
